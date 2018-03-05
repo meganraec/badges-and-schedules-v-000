19308d9c335f5ba72249c459adbbd1aace8637e3
Megan Collier <meganraec@live.com>
2018-03-05 00:28:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,7 @@ def assign_rooms(attendees)
   room_assignments = []
   # attendees.each_with_index do |name, index| index += 1
   #   room_assignments << "Hello, #{name}! You'll be assigned to room #{index}!"}
-  end
+  # end
   return room_assignments
 end
 
