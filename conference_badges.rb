4abb7ff34d51e81bf20591ff721be662b3c45adf
Megan Collier <meganraec@live.com>
2018-03-05 00:34:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(attendees)
   return room_assignments
 end
 
-def printer
+def printer(attendees)
   puts "#{batch_badge_creator(attendees)}"
   puts "#{assign_rooms(attendees)}"
 end
